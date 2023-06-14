3f9eb6d98f276f7a19be914203b370f46c49ee4d
Andrea Colzani <67199271+andrecolz@users.noreply.github.com>
2023-06-14 09:31:28 +0200

    Add files via upload

--- ScanFileGUI/ScanFile/obj/Debug/ScanFile_Content.g.i.cs
+++ ScanFileGUI/ScanFile/obj/Debug/ScanFile_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("time-left.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("compare.png")]
 
 
